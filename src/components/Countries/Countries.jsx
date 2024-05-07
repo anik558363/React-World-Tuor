04b17e556e0099ee8af82ac723d44b76f48c6453
anik558363 <anikmondol558363@gmil.com>
2024-05-07 17:40:30 +0600

    toggle state based onClick and conditional rending

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,25 +1,27 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
-
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
 
     console.log(countries);
 
     return (
         <div>
             <h3>Countries {countries.length}</h3>
-            {
-                countries.map((country, index) => <Country country={country} key={index}></Country>)
-            }
+            <div className="country_container">
+                {
+                    countries.map((country, index) => <Country country={country} key={index}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import './Country.css'
 
 
-const Country = ({country}) => {
-    const {name, flags} = country;
+
+const Country = ({ country }) => {
+
+
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = () => {
+        setVisited(!visited)
+    }
+
+
+
+  
+
+
+    const { name, flags, population, area, capital, cca3 } = country;
     return (
         <div className='country'>
             <h4>Name: {name?.common}</h4>
             <img className='img' src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+            <p>Capital: {capital ? capital : "Unknown"}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisited} className='mb'>{visited ? "Visited" : "Going" }</button>
         </div>
     );
 };
