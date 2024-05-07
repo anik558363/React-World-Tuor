1f93a897d76b1de1a5364d1c7789c9880bb7b0c3
anik558363 <anikmondol558363@gmil.com>
2024-05-07 21:39:25 +0600

    upadting state in array and pass parameter

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -3,7 +3,7 @@ import './Country.css'
 
 
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
 
 
     const [visited, setVisited] = useState(false)
@@ -13,20 +13,16 @@ const Country = ({ country }) => {
     }
 
 
-
-  
-
-
     const { name, flags, population, area, capital, cca3 } = country;
     return (
-        <div className='country'>
+        <div className={`country ${visited ?'visited' : 'visited1'}`}>
             <h4>Name: {name?.common}</h4>
             <img className='img' src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p>Capital: {capital ? capital : "Unknown"}</p>
             <p><small>Code: {cca3}</small></p>
-            <button onClick={handleVisited} className='mb'>{visited ? "Visited" : "Going" }</button>
+            <button onClick={() => { handleVisitedCountry(country); handleVisited(); }} className='mb'>{visited ? "Visited" : "Going" }</button>
         </div>
     );
 };
